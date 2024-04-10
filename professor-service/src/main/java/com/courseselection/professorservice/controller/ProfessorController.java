42cc67dbf526fe3dd64157ddd2bba7beb7e9f64a
barisayyildiz <99ayyldzbaris99@gmail.com>
2024-04-10 18:49:09 +0300

    feat: signup route implemented

--- professor-service/src/main/java/com/courseselection/professorservice/controller/ProfessorController.java
+++ professor-service/src/main/java/com/courseselection/professorservice/controller/ProfessorController.java
@@ -1,14 +1,23 @@
 package com.courseselection.professorservice.controller;
 
+import com.courseselection.professorservice.dtos.SignupRequestDto;
+import com.courseselection.professorservice.model.Professor;
+import com.courseselection.professorservice.service.AuthenticationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
 public class ProfessorController {
+    @Autowired
+    private AuthenticationService authenticationService;
 
-    @PostMapping("/api/signup")
-    public String signup() {
-        return "hello world";
+    @PostMapping("/auth/signup")
+    public ResponseEntity<Professor> signup(@RequestBody SignupRequestDto signupRequestDto) {
+        return new ResponseEntity<>(authenticationService.signup(signupRequestDto), HttpStatus.OK);
     }
 
 }
--- professor-service/src/main/java/com/courseselection/professorservice/model/Professor.java
+++ professor-service/src/main/java/com/courseselection/professorservice/model/Professor.java
@@ -1,10 +1,7 @@
 package com.courseselection.professorservice.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
@@ -22,6 +19,8 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Getter
+@Setter
 public class Professor implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
